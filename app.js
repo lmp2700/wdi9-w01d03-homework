db8a30fc6f645a20d82b2ba46097ef892f03545f
Liz Steak <lizmaepark@gmail.com>
2018-09-12 23:16:16 -0600

    FizzBuzz Part 1 completed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 // "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
 // Hint: You will need to use Math.random()
 
-const catTalk = ["Love me, pet me! HSSSSSS!"];
+/* const catTalk = ["Love me, pet me! HSSSSSS!"];
 const catPhrase = ["...human...why you taking pictures of me?...", 
                    "...the catnip made me do it...", "...why does the red dot always get away..."];
 for(i= 0; i < 20; i++) {
@@ -21,5 +21,17 @@ for(i= 0; i < 20; i++) {
     // below code is not working, need to research more
 } if(i % 2 === 0) {
     console.log(Math.random(catPhrase));
-}
+} */
 
+// need to remove # when there is a phrase
+for(i = 1; i <= 100; i++) {
+    console.log(i); {
+        if(i % 3 === 0) {
+            console.log("Fizz");
+        }   if(i % 5 === 0) {
+            console.log("Buzz");
+        } if(i % 3 === 0 && i % 5 === 0) {
+            console.log("Fizzbuzz");
+        }
+    }  
+};
