fafcc86a358cd759f3b5b0f6c2620d0a5ac1055b
Liz Steak <lizmaepark@gmail.com>
2018-09-16 19:21:04 -0600

    Commit 2 - Get Even answered

--- app.js
+++ app.js
@@ -2,29 +2,24 @@
 //     console.log(i);
 // }
 
-// const num =[];
-// for(num = 0; num <= 200; num++) {
-//     if(num % 2 === 0);
-//     console.log(num);
+for(let i = 0; i <= 200; i += 2) {
+    console.log(i);
+}
+
+
+//not spitting out random phrase
+// const catPhrase = ["...human...why you taking pictures of me?...", 
+//                    "...the catnip made me do it...", "...why does the red dot always get away..."];
+// for(i = 1; i <= 20; i++) {
+//     if(i % 2 === 0) {
+//     let randomPhrase = Math.floor(Math.random() * catPhrase.length);
+// } else {
+//     console.log("Love me, pet me! HSSSSSS!");
+//   }
 // }
 
-
-// For every even number in your loop, log "...human...why you taking pictures of me?...", 
-// "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
-// Hint: You will need to use Math.random()
-
-/* const catTalk = ["Love me, pet me! HSSSSSS!"];
-const catPhrase = ["...human...why you taking pictures of me?...", 
-                   "...the catnip made me do it...", "...why does the red dot always get away..."];
-for(i= 0; i < 20; i++) {
-    console.log(catTalk);
-    // below code is not working, need to research more
-} if(i % 2 === 0) {
-    console.log(Math.random(catPhrase));
-} */
-
 // need to remove # when there is a phrase
-for(i = 1; i <= 100; i++) {
+/* for(i = 1; i <= 100; i++) {
     console.log(i); {
         if(i % 3 === 0) {
             console.log("Fizz");
@@ -34,4 +29,44 @@ for(i = 1; i <= 100; i++) {
             console.log("Fizzbuzz");
         }
     }  
-};
+}; */
+
+
+// const shahzad = ["Shahzad", 1000, "Austin"];
+// const jim = ["Jim", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const daniel = ["Daniel", 186, "Dallas"];
+// const ryan = ["Ryan", 65, "Denver"];
+
+// Daniel decides that Shahzad can't be named "Shahzad" anymore. 
+// Remove "Shahzad" from the shahzad array and replace it with "Gameboy".
+
+// Daniel just had his birthday; change Daniel's array to reflect him being a year older. 
+// Don't just hard code 187--pretend that you don't know that his age is 186, and write your 
+// code to just make him a year older than whatever age he was.
+
+// Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+
+// Reuben left Durham 5 years ago to come to Chicago. Remove "Durham" from Reuben's array and add "Chicago". 
+// (Note: remove and then add is different from simply changing the value at that index.)
+
+// Jim could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 
+// cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, 
+// but see if you can do it in one line of code
+
+// Bonus: Whoops! Jim is actually only allowed to be in one of two cities. Whatever the first of the 
+// 3 cities for Jim is now, remove it from the array using .splice()
+
+
+
+
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+// let sum = 0;
+// for(i = 0; i <= 1000; i++) {
+//     if(i % 3 === 0) || i % 5 === 0) {
+//        sum += i; // sum = sum + i 
+//     }
+// };
+// console.log(sum);
